93c6485d06843261cf2d62fde8e9ec7e2e066886
mfritz10 <fritzm25@mail.wlu.edu>
2022-12-01 21:59:42 -0500

    Exponent analyzer and exponent functionality added.

---
+++ src/picasso/parser/ExpAnalyzer.java
@@ -0,0 +1,20 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Exponent;
+import picasso.parser.tokens.Token;
+
+public class ExpAnalyzer extends UnaryFunctionAnalyzer {
+	
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Need to remove the exponent token
+		// the parameter is the next token on the stack.
+		// But, it needs to be processed
+		return new Exponent(SemanticAnalyzer.getInstance().generateExpressionTree(
+				tokens));
+	}
+
+}
