cf3bf5b9f28264ecc83212ed62bb2d0c3321f2b0
Harry <crutcherh23@mail.wlu.edu>
2022-12-15 05:06:53 -0600

    Added random function

---
+++ src/picasso/parser/RandAnalyzer.java
@@ -0,0 +1,24 @@
+package picasso.parser;
+
+import java.util.Stack;
+
+import picasso.parser.language.ExpressionTreeNode;
+import picasso.parser.language.expressions.Rand;
+import picasso.parser.tokens.Token;
+/**
+ * Handles parsing the exponent function.
+ * 
+ * @author Grace Owens
+ * 
+ */
+public class RandAnalyzer extends UnaryFunctionAnalyzer {
+	
+	@Override
+	public ExpressionTreeNode generateExpressionTree(Stack<Token> tokens) {
+		tokens.pop(); // Need to remove the exponent token
+		// the parameter is the next token on the stack.
+		// But, it needs to be processed
+		return new Rand();
+	}
+
+}
---
+++ src/picasso/parser/tokens/functions/RandToken.java
@@ -0,0 +1,9 @@
+package picasso.parser.tokens.functions;
+
+public class RandToken extends FunctionToken {
+
+	public RandToken() {
+		super("Rand Function Token");
+	}
+
+}
